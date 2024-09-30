5fbb72ceb56b6cc5b5920c41da8d4d3b8546ca11
Caleb Stein <caleb@calebstein.net>
2024-09-29 21:28:37 -0700

    update demo sprites

--- src/game_loop.c
+++ src/game_loop.c
@@ -7,52 +7,54 @@
 #include "engine/input.h"
 #include "engine/render.h"
 
+#define NUM_PLAYER_IDLE_FRAMES 2
+
 /* Game globals */
 struct sprite_slot_t *p_char = NULL;
-struct sprite_slot_t *p_char2 = NULL;
+struct sprite_slot_t *building = NULL;
+
+int player_idle_frames[NUM_PLAYER_IDLE_FRAMES] = { 1, 3 };
+int player_idle_idx = 0;
 bool walking = false;
 
 void
 game_init(void) {
-	init_sprite_slot(&p_char, 1, 3, 4, 64, 64, false);
-	init_sprite_slot(&p_char2, 16, 1, 1, 48, 32, false);
+	init_sprite_slot(&p_char, player_idle_frames[player_idle_idx], 2, 4, 24, 196, false);
+	init_sprite_slot(&building, 12, 4, 8, 96, 148, false);
 }
 
 void
 game_loop(Uint64 frame, const struct input_state_t *key_actions, struct save_data_t *save_data) {
-	static Uint64 walk_timer = 0;
 	static Vector2 camera_location;
+	static Uint64 idle_anim = 0;
 
 	(void)save_data;
 
 	walking=false;
 	if (key_actions->left) {
-		p_char2->flip = true;
-		if (p_char2->x_subp <= 3) {
-			p_char2->x--;
+		p_char->flip = true;
+		if (p_char->x_subp <= 3) {
+			p_char->x--;
 		}
-		p_char2->x_subp -= 4;
+		p_char->x_subp -= 4;
 		walking = true;
 	}
 	if (key_actions->right) {
-		p_char2->flip = false;
-		if(p_char2->x_subp >= 12) {
-			p_char2->x++;
+		p_char->flip = false;
+		if(p_char->x_subp >= 12) {
+			p_char->x++;
 		}
-		p_char2->x_subp += 4;
+		p_char->x_subp += 4;
 		walking = true;
 	}
 
-	if (frame - walk_timer > 15) {
-		if (walking) {
-			p_char2->num = p_char2->num == 16 ? 32 : 16;
-		} else {
-			p_char2->num = 16;
-		}
-		walk_timer = frame;
+	if (frame - idle_anim > 30) {
+		if (++player_idle_idx >= NUM_PLAYER_IDLE_FRAMES) player_idle_idx = 0;
+		p_char->num = player_idle_frames[player_idle_idx];
+		idle_anim = frame;
 	}
 
-	camera_location = get_sprite_center(p_char2);
+	camera_location = get_sprite_center(p_char);
 	camera_location.x -= get_window_size().x / 2;
 	camera_location.y = 0;
 	set_camera(camera_location);
