4d54d632c9d331ff4b9f5eb68681aec8136b0bb8
Caleb Stein <caleb@calebstein.net>
2024-10-13 17:18:02 -0700

    fixed width ints for input handler

--- src/engine/input.c
+++ src/engine/input.c
@@ -3,7 +3,7 @@
 #include "config.h"
 
 /* Private function prototypes */
-unsigned int rotate_left(unsigned int n, int c); /* Rotate the bits of n to the left by c bits (MSB becomes LSB) */
+Uint64 rotate_left(Uint64 n, Uint32 c); /* Rotate the bits of n to the left by c bits (MSB becomes LSB) */
 
 /* File-scoped variables */
 static SDL_Keycode key1[] = {
@@ -20,7 +20,7 @@ static SDL_Keycode key2[] = {
 
 void
 handle_keydown(union input_state_u *key_actions, const SDL_Event *e) {
-	int i;
+	Uint32 i;
 
 	for (i = 0; i < ACTION_COUNT; i++) {
 		if (e->key.keysym.sym == key1[i]) {
@@ -38,8 +38,8 @@ handle_keydown(union input_state_u *key_actions, const SDL_Event *e) {
 
 void
 handle_keyup(union input_state_u *key_actions, const SDL_Event *e) {
-	int i;
-	unsigned int mask = 0xfffffffe;
+	Uint32 i;
+	Uint64 mask = 0xfffffffffffffffe;
 
 	for (i = 0; i < ACTION_COUNT; i++) {
 		if (e->key.keysym.sym == key1[i]) {
@@ -59,12 +59,9 @@ handle_keyup(union input_state_u *key_actions, const SDL_Event *e) {
  * Private functions
  */
 
-unsigned int
-rotate_left(unsigned int n, int c) {
-	int int_size = sizeof(unsigned int) * 8;
-
+Uint64
+rotate_left(Uint64 n, Uint32 c) {
 	if (c == 0) return n;
-	c %= int_size;
 
-	return (n << c) | (n >> (int_size - c));
+	return (n << c) | (n >> (64 - c));
 }
