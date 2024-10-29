9fae68446b687a65feb142c280d9be851c7d567e
Caleb Stein <caleb@calebstein.net>
2024-10-28 17:10:44 -0700

    update example
    Signed-off-by: Caleb Stein <caleb@calebstein.net>

--- src/game_loop.c
+++ src/game_loop.c
@@ -3,11 +3,8 @@
 enum player_state_e {
 	idle,
 	walk,
-	atk
-};
-
-enum player_facing_e {
-	n, s, e, w
+	atk,
+	ko
 };
 
 /* Game globals */
@@ -18,25 +15,11 @@ AmphoraMessage *hello;
 AmphoraMessage *timer;
 AmphoraMessage *stationary;
 enum player_state_e p_state = idle;
-enum player_facing_e p_facing = s;
 
 void
 end_player_attack(void) {
 	p_state = idle;
-	switch (p_facing) {
-		case n:
-			set_frameset(player, "IdleUp");
-			break;
-		case s:
-			set_frameset(player, "IdleDown");
-			break;
-		case e:
-			set_frameset(player, "IdleRight");
-			break;
-		case w:
-			set_frameset(player, "IdleLeft");
-			break;
-	}
+	set_frameset(player, "Idle");
 }
 
 void
@@ -50,18 +33,10 @@ game_init(void) {
 	init_sprite_slot(&heart, "Objects", -96, 32, 2, false, true, 11);
 	init_sprite_slot(&rotating_heart, "Objects", 128, 72, 3, false, false, -1);
 
-	add_frameset(player, "IdleDown", 0, 5, 16, 24, 0, 0, 1, 0);
-	add_frameset(player, "WalkDown", 0, 5, 16, 24, 0, 0, 4, 30);
-	add_frameset(player, "IdleRight", 0, 37, 16, 24, 0, 0, 1, 0);
-	add_frameset(player, "WalkRight", 0, 37, 16, 24, 0, 0, 4, 30);
-	add_frameset(player, "IdleUp", 0, 69, 16, 24, 0, 0, 1, 0);
-	add_frameset(player, "WalkUp", 0, 69, 16, 24, 0, 0, 4, 30);
-	add_frameset(player, "IdleLeft", 0, 101, 16, 24, 0, 0, 1, 0);
-	add_frameset(player, "WalkLeft", 0, 101, 16, 24, 0, 0, 4, 30);
-	add_frameset(player, "AttackDown", 0, 132, 32, 24, 8, 0, 4, 5);
-	add_frameset(player, "AttackUp", 0, 164, 32, 24, 8, 0, 4, 5);
-	add_frameset(player, "AttackRight", 0, 196, 32, 24, 8, 0, 4, 5);
-	add_frameset(player, "AttackLeft", 0, 228, 32, 24, 8, 0, 4, 5);
+	add_frameset(player, "Idle", 0, 17, 32, 48, 0, 0, 1, 0);
+	add_frameset(player, "Walk", 32, 17, 32, 48, 0, 0, 6, 30);
+	add_frameset(player, "Attack", 223, 145, 32, 48, 0, 0, 2, 15);
+	add_frameset(player, "KO", 81, 355, 48, 32, 8, -8, 2, 30);
 
 	add_frameset(heart, "Default", 63, 0, 16, 16, 0, 0, 1, 0);
 	add_frameset(rotating_heart, "Rotate", 64, 129, 16, 16, 0, 0, 4, 15);
@@ -83,71 +58,38 @@ game_loop(Uint64 frame, const struct input_state_t *key_actions, SaveData *save_
 
 	camera_location = get_camera();
 
-	if (key_actions->left && p_state != atk) {
+	if (key_actions->left && p_state != atk && p_state != ko) {
 		p_state = walk;
-		set_frameset(player, "WalkLeft");
+		set_frameset(player, "Walk");
 		player_speed = key_actions->dash ? 2 : 1;
-		set_frameset_delay(player, "WalkLeft", key_actions->dash ? 15 : 30);
+		flip_sprite(player);
+		set_frameset_delay(player, "Walk", key_actions->dash ? 15 : 30);
 		move_sprite(player, -player_speed, 0);
-		p_facing = w;
 	}
-	if (key_actions->right && p_state != atk) {
+	if (key_actions->right && p_state != atk && p_state != ko) {
 		p_state = walk;
-		set_frameset(player, "WalkRight");
+		set_frameset(player, "Walk");
 		player_speed = key_actions->dash ? 2 : 1;
-		set_frameset_delay(player, "WalkRight", key_actions->dash ? 15 : 30);
+		unflip_sprite(player);
+		set_frameset_delay(player, "Walk", key_actions->dash ? 15 : 30);
 		move_sprite(player, player_speed, 0);
-		p_facing = e;
-	}
-	if (key_actions->up && p_state != atk) {
-		p_state = walk;
-		set_frameset(player, "WalkUp");
-		player_speed = key_actions->dash ? 2 : 1;
-		set_frameset_delay(player, "WalkUp", key_actions->dash ? 15 : 30);
-		move_sprite(player, 0, -player_speed);
-		p_facing = n;
-	}
-	if (key_actions->down && p_state != atk) {
-		p_state = walk;
-		set_frameset(player, "WalkDown");
-		player_speed = key_actions->dash ? 2 : 1;
-		set_frameset_delay(player, "WalkDown", key_actions->dash ? 15 : 30);
-		move_sprite(player, 0, player_speed);
-		p_facing = s;
 	}
 	if (key_actions->attack && p_state != atk) {
-		p_state = atk;
-		switch (p_facing) {
-			case n:
-				play_oneshot(player, "AttackUp", end_player_attack);
-				break;
-			case s:
-				play_oneshot(player, "AttackDown", end_player_attack);
-				break;
-			case e:
-				play_oneshot(player, "AttackRight", end_player_attack);
-				break;
-			case w:
-				play_oneshot(player, "AttackLeft", end_player_attack);
-				break;
+		if (p_state == ko) {
+			p_state = idle;
+			set_frameset(player, "Idle");
+		} else {
+			p_state = atk;
+			play_oneshot(player, "Attack", end_player_attack);
 		}
 	}
-	if (!key_actions->left && !key_actions->right && !key_actions->up && !key_actions->down && p_state == walk) {
+	if (!key_actions->left && !key_actions->right && p_state == walk) {
 		p_state = idle;
-		switch (p_facing) {
-			case n:
-				set_frameset(player, "IdleUp");
-				break;
-			case s:
-				set_frameset(player, "IdleDown");
-				break;
-			case e:
-				set_frameset(player, "IdleRight");
-				break;
-			case w:
-				set_frameset(player, "IdleLeft");
-				break;
-		}
+		set_frameset(player, "Idle");
+	}
+	if (key_actions->ko && p_state != ko) {
+		p_state = ko;
+		play_oneshot(player, "KO", NULL);
 	}
 	if (key_actions->zoom) {
 		set_camera_zoom(150, 60);
