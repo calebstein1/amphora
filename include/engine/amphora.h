96ec13c72fe4e0aba3137884b03fb8bb70b088ae
Caleb Stein <caleb@calebstein.net>
2024-11-14 08:58:56 -0800

    add (undocumented) ability to expose internal definitions and functions to game

--- include/engine/amphora.h
+++ include/engine/amphora.h
@@ -1,6 +1,19 @@
 #ifndef AMPHORA_H
 #define AMPHORA_H
 
+#include "config.h"
+
+#ifdef EXPOSE_INTERNAL_DEFINITIONS_AND_FUNCTIONS
+#include "engine/internal/events.h"
+#include "engine/internal/img.h"
+#include "engine/internal/input.h"
+#include "engine/internal/render.h"
+#include "engine/internal/tilemap.h"
+#include "engine/internal/timer.h"
+#include "engine/internal/ttf.h"
+#include "engine/game_loop.h"
+#include "engine/util.h"
+#else
 #include "engine/events.h"
 #include "engine/game_loop.h"
 #include "engine/img.h"
@@ -9,5 +22,6 @@
 #include "engine/tilemap.h"
 #include "engine/ttf.h"
 #include "engine/util.h"
+#endif
 
 #endif /* AMPHORA_H */
