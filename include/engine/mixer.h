3f183975a9db17cd1d1de6fa3243c0a209216bcd
Caleb Stein <caleb@calebstein.net>
2024-11-25 19:36:00 -0800

    upadte comments

--- include/engine/mixer.h
+++ include/engine/mixer.h
@@ -25,12 +25,19 @@ extern "C" {
 #endif
 /* Play a sound effect */
 void play_sfx(const char *name, int channel, int repeat);
+/* Sets the current music track */
 void set_music(const char *name);
+/* Play the current set music track with a fade-in, looping infinitely */
 void play_music(int ms);
+/* Play the current set music track with a fade-in, looping n times */
 void play_music_n(int n, int ms);
+/* Pause the currently playing music track */
 void pause_music(void);
+/* Unpause the currently playing music track if paused */
 void unpause_music(void);
+/* Stop the currently playing music track and free its resources immediately */
 void stop_music(void);
+/* Stop the currently playing music track and free its resources after a fade-out */
 void fade_music(int ms);
 #ifdef __cplusplus
 };
