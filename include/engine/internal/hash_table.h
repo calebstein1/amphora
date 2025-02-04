08cfd455ce29762aca7eadc8ac083be6bb4d14d5
Caleb Stein <caleb@calebstein.net>
2025-02-03 19:08:39 -0800

    fix hash_table header

--- include/engine/internal/hash_table.h
+++ include/engine/internal/hash_table.h
@@ -5,12 +5,15 @@
 
 #define Amphora_HTZeroTable(t) SDL_memset((t), 0, sizeof((t)))
 #define Amphora_HTGetValue(k, type, t) *(type *)Amphora_HTGetValuePtr((k), (t), (sizeof((t)) / sizeof(*(t))))
-#define Amphora_HTSetValue(k, type, v, t) Amphora_HTSetValuePtr((k), (uintptr_t)(v), sizeof(type), (t), (sizeof((t)) / sizeof(*(t))))
+#define Amphora_HTSetValue(k, type, v, t) Amphora_HTSetValuePtr((k), (Uint64)(v), sizeof(type), (t), (sizeof((t)) / sizeof(*(t))))
 
 typedef struct {
     Uint64 d;
     Uint32 h;
     Uint32 b;
 } HT_HashTable;
+
+void *Amphora_HTGetValuePtr(const char *key, const HT_HashTable *t, size_t len);
+Uint32 Amphora_HTSetValuePtr(const char *key, Uint64 val, size_t nbytes, HT_HashTable *t, size_t len);
 
 #endif //AMPHORA_HASH_TABLE_INTERNAL_H
