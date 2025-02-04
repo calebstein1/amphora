84a3d5cc90abe85996c74abbed83d318fb57028c
Caleb Stein <caleb@calebstein.net>
2025-02-03 19:53:04 -0800

    hash table fixes
    Signed-off-by: Caleb Stein <caleb@calebstein.net>

--- src/engine/hash_table.c
+++ src/engine/hash_table.c
@@ -2,8 +2,8 @@
 #include "engine/internal/lib.h"
 
 /* Prototypes for private functions */
-int Amphora_HTProbeForBucket(const HT_HashTable *t, Uint32 hash, int i, int len);
-int Amphora_HTProbeForFreeBucket(const HT_HashTable *t, int i, int len);
+static int Amphora_HTProbeForBucket(const HT_HashTable *t, Uint32 hash, int i, int len);
+static int Amphora_HTProbeForFreeBucket(const HT_HashTable *t, int i, int len);
 
 /*
  * Internal functions
@@ -11,9 +11,9 @@ int Amphora_HTProbeForFreeBucket(const HT_HashTable *t, int i, int len);
 
 void *
 Amphora_HTGetValuePtr(const char *key, const HT_HashTable *t, int len) {
-	Uint32 hash = Amphora_crc32(key);
+	const Uint32 hash = Amphora_crc32(key);
 	int i = (int)(hash % len);
-	size_t k_len = SDL_strlen(key);
+	const size_t k_len = SDL_strlen(key);
 
 	if (t[i].h == hash && SDL_memcmp(&t[i].b, key, k_len > sizeof(Uint32) ? sizeof(Uint32) : k_len) == 0)
 		return (void *)&t[i].d;
@@ -25,15 +25,15 @@ Amphora_HTGetValuePtr(const char *key, const HT_HashTable *t, int len) {
 
 Uint32
 Amphora_HTSetValuePtr(const char *key, Uint64 val, size_t nbytes, HT_HashTable *t, int len) {
-	Uint32 hash = Amphora_crc32(key);
+	const Uint32 hash = Amphora_crc32(key);
 	int i = (int)(hash % len);
-	size_t k_len = SDL_strlen(key);
+	const size_t k_len = SDL_strlen(key);
 
-	if (nbytes > sizeof(Uint64)) return -1;
+	if (nbytes > sizeof(Uint64)) return 1;
 
 	if (t[i].h && (t[i].h != hash || SDL_memcmp(&t[i].b, key, k_len > sizeof(Uint32) ? sizeof(Uint32) : k_len) != 0))
 		i = Amphora_HTProbeForFreeBucket(t, i, len);
-	if (i == -1) return 0;
+	if (i == -1) return 1;
 
 	t[i].h = hash;
 	t[i].d = val;
@@ -46,9 +46,9 @@ Amphora_HTSetValuePtr(const char *key, Uint64 val, size_t nbytes, HT_HashTable *
  * Private functions
  */
 
-int
+static int
 Amphora_HTProbeForBucket(const HT_HashTable *t, Uint32 hash, int i, int len) {
-	int s = i ? i - 1 : len - 1;
+	const int s = i ? i - 1 : len - 1;
 
 	while (t[i].h != hash) {
 		if (++i == len) i = 0;
@@ -58,9 +58,9 @@ Amphora_HTProbeForBucket(const HT_HashTable *t, Uint32 hash, int i, int len) {
 	return i;
 }
 
-int
+static int
 Amphora_HTProbeForFreeBucket(const HT_HashTable *t, int i, int len) {
-	int s = i ? i - 1 : len - 1;
+	const int s = i ? i - 1 : len - 1;
 
 	while (t[i].h) {
 		if (++i == len) i = 0;
