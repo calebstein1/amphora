ef23d3425f8e710a91a65cd43479f79f58ca8cf0
Caleb Stein <caleb@calebstein.net>
2025-04-26 04:00:24 -0700

    snowfall optimization

--- src/FX/snowfall.c
+++ src/FX/snowfall.c
@@ -7,8 +7,7 @@ Snowfall(AmphoraParticle *particle, const SDL_FRect *rect) {
 		particle->vx = Amphora_GetRandomF() - 0.5f;
 		particle->vy = Amphora_GetRandomF();
 		particle->data1 = particle->x;
-		if (particle->vx > 0) particle->data3 = 1;
-		else particle->data3 = -1;
+		particle->data3 = particle->vx > 0 ? 1 : 0;
 	}
 
 	particle->y += particle->vy;
