ed24f75501ad793f648e72f8d9122f8108dce6b2
Caleb Stein <caleb@calebstein.net>
2025-04-25 17:06:44 -0700

    add inline documentation

--- include/engine/particles.h
+++ include/engine/particles.h
@@ -13,7 +13,9 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
+/* Create a new particle emitter */
 AmphoraEmitter *Amphora_CreateEmitter(float x, float y, float w, float h, float start_x, float start_y, int spread_x, int spread_y, int count, float p_w, float p_h, SDL_Color color, bool stationary, Sint32 order, void (*update_fn)(AmphoraParticle *, SDL_FRect));
+/* Destroy a particle emitter */
 int Amphora_DestroyEmitter(AmphoraEmitter *emitter);
 #ifdef __cplusplus
 }
