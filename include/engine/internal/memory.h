4252000052db1f9720c2b380f52c2e20f4c8b833
Caleb Stein <caleb@calebstein.net>
2025-08-11 16:04:51 -0700

    fill out alloc functions

--- include/engine/internal/memory.h
+++ include/engine/internal/memory.h
@@ -5,14 +5,20 @@
 
 #define AMPHORA_NUM_MEM_BLOCKS 0x100
 
-typedef char AmphoraMemBlock[0x10000];
+typedef Uint8 AmphoraMemBlock[0x10000];
 
-struct amphora_mem_block_metadata {
+struct amphora_mem_block_metadata_t {
 	unsigned short addr;
 	unsigned short allocations;
 };
 
 int Amphora_InitHeap(void);
 void Amphora_DestroyHeap(void);
+void *Amphora_HeapAlloc(size_t size);
+void *Amphora_HeapAllocFrame(size_t size);
+void *Amphora_HeapRealloc(void *ptr, size_t nbytes, size_t size);
+void *Amphora_HeapCalloc(size_t num, size_t size);
+char *Amphora_HeapStrdup(const char *str);
+void Amphora_HeapFree(void *ptr);
 
 #endif /* MEMORY_INTERNAL_H */
--- src/engine/memory.c
+++ src/engine/memory.c
@@ -9,6 +9,8 @@
 
 /* File-scoped variables */
 static AmphoraMemBlock *amphora_heap;
+static struct amphora_mem_block_metadata_t heap_metadata[AMPHORA_NUM_MEM_BLOCKS];
+static Uint8 current_block = 0;
 
 int
 Amphora_InitHeap(void) {
@@ -17,6 +19,11 @@ Amphora_InitHeap(void) {
 #else
 	amphora_heap = malloc(sizeof(AmphoraMemBlock) * AMPHORA_NUM_MEM_BLOCKS);
 #endif
+
+	if (amphora_heap == NULL) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Failed to initialize heap");
+		return AMPHORA_STATUS_ALLOC_FAIL;
+	}
 	return AMPHORA_STATUS_OK;
 }
 
@@ -32,15 +39,87 @@ Amphora_DestroyHeap(void) {
 
 void *
 Amphora_HeapAlloc(size_t size) {
-	return NULL;
+	Uint8 *addr;
+	int i = 0;
+	size_t aligned_size = (size + 7) & ~7;
+
+	if (size > sizeof(AmphoraMemBlock)) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Allocation cannot exceed %d", sizeof(AmphoraMemBlock));
+		return NULL;
+	}
+	while (heap_metadata[current_block].addr + aligned_size >= sizeof(AmphoraMemBlock)) {
+		current_block++;
+		SDL_Log("Incrementing current block to %d\n", current_block);
+		if (++i < AMPHORA_NUM_MEM_BLOCKS) continue;
+
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Heap full");
+		return NULL;
+	}
+	addr = &amphora_heap[current_block][heap_metadata[current_block].addr];
+	heap_metadata[current_block].addr += aligned_size;
+	heap_metadata[current_block].allocations++;
+
+	return addr;
 }
 
 void *
 Amphora_HeapAllocFrame(size_t size) {
+	/*
+	 * TODO: Like HeapAlloc but auto-free on the next render frame
+	 */
 	return NULL;
+}
+
+void *
+Amphora_HeapRealloc(void *ptr, size_t nbytes, size_t size) {
+	Uint8 *addr = Amphora_HeapAlloc(size);
+
+	if (addr == NULL) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Failed to reallocate space on heap");
+		return ptr;
+	}
+	(void)memcpy(addr, ptr, nbytes);
+	Amphora_HeapFree(ptr);
+
+	return addr;
+}
+
+void *
+Amphora_HeapCalloc(size_t num, size_t size) {
+	Uint8 *addr = Amphora_HeapAlloc(num * size);
+
+	if (addr == NULL) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Failed to reallocate space on heap");
+		return NULL;
+	}
+	(void)memset(addr, 0, num * size);
+
+	return addr;
+}
+
+char *
+Amphora_HeapStrdup(const char *str) {
+	char *addr = Amphora_HeapAlloc(strlen(str) + 1);
+
+	if (addr == NULL) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Failed to reallocate space on heap");
+		return NULL;
+	}
+	(void)strcpy(addr, str);
+
+	return addr;
 }
 
 void
 Amphora_HeapFree(void *ptr) {
+	const long idx = (Uint8 *)ptr - &amphora_heap[0][0];
+	unsigned int block;
 
+	if (idx < 0 || idx > sizeof(AmphoraMemBlock) * AMPHORA_NUM_MEM_BLOCKS) {
+		Amphora_SetError(AMPHORA_STATUS_ALLOC_FAIL, "Address supplied outside heap range");
+		return;
+	}
+	block = idx / sizeof(AmphoraMemBlock);
+	if (--heap_metadata[block].allocations == 0)
+		heap_metadata[block].addr = 0;
 }
