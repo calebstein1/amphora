cd078252ca5614f37e78eb2a0d6744f86a8036fa
Caleb Stein <caleb@calebstein.net>
2025-08-11 01:24:34 -0700

    explicitly ignore return

--- src/engine/lib.c
+++ src/engine/lib.c
@@ -9,7 +9,7 @@ Amphora_ConcatString(char **s1, const char *s2) {
 	size_t n = SDL_strlen(*s1) + SDL_strlen(s2) + 1;
 
 	*s1 = SDL_realloc(*s1, n);
-	SDL_strlcat(*s1, s2, n);
+	(void)SDL_strlcat(*s1, s2, n);
 
 	return *s1;
 }
